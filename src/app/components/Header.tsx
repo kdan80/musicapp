0920a449377159f61d92ffd88e71d4f2b201f1e6
kdan80 <kierandansey80@gmail.com>
2023-11-08 12:54:51 +0000

    added logout modal

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,29 +1,33 @@
 import React from 'react'
+import LogoutModal from './LogoutModal'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
-    const setModalIsOpen = (bool: boolean) => {
-        const mess = bool ? 'modal open' : 'modal closed'
-        alert(mess)
-    }
+    const [modalIsOpen, setModalIsOpen] = React.useState(false)
 
     return (
-        <header className='w-full bg-[#202020] px-6 py-4'>
+        <header className='w-full bg-[#202020] py-4 px-12'>
             <div className='flex items-center justify-between'>
                 <div className='font-[1.1rem] font-semibold text-[#169b45]'>MusicApp</div>
                 <button
-                    className='flex items-center gap-4'
+                    className='flex items-center gap-4 '
                     title='user'
                     onClick={() => setModalIsOpen(true)}
                     type='button'
                 >
-                    <div className='bg-[#169b45] w-[32px] h-[32px] rounded-full flex justify-center items-center'>
-                        {/* <FontAwesomeIcon icon={faUser} /> */}xx
+                    <div className='bg-[#169b45] w-[32px] h-[32px] rounded-full flex justify-center items-center hover:bg-[#1ab751] transition-colors duration-300 ease-in-out'>
+                        <FontAwesomeIcon icon={faUser} />
                     </div>
-                    <div className='hidden md:block hover:text-[#169b45] transition-colors duration-300 ease-in-out'>
+                    <div className='hidden md:block hover:text-[#1ab751] transition-colors duration-300 ease-in-out'>
                         guest
                     </div>
                 </button>
             </div>
+            <LogoutModal
+                setModalIsOpen={setModalIsOpen}
+                modalIsOpen={modalIsOpen}
+            />
         </header>
     )
 }
