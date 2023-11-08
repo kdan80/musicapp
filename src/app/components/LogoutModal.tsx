802898ccd382d43024187ad8d3073427d561e36f
kdan80 <kierandansey80@gmail.com>
2023-11-08 12:55:15 +0000

    added logout modal

---
+++ src/app/components/LogoutModal.tsx
@@ -0,0 +1,53 @@
+import React, { Dispatch, SetStateAction } from 'react'
+
+interface Props {
+    setModalIsOpen: Dispatch<SetStateAction<boolean>>
+    modalIsOpen: boolean
+}
+
+// .btnContainer {
+//     display: flex;
+//     justify-content: flex-end;
+//     align-items: center;
+//     gap: 3rem;
+//     margin-top: 1rem;
+
+//     button {
+//         font-size: 1.1rem;
+//         font-weight: 600;
+//         color: colors.$txt-hero;
+//     }
+
+const LogoutModal = ({ setModalIsOpen, modalIsOpen }: Props) => {
+    return (
+        <>
+            {modalIsOpen && (
+                <div
+                    // ref={modalRef}
+                    className='absolute top-12 left-0 right-0 z-20 ml-auto mr-auto min-w-[15rem] max-w-[18rem] md:min-w-[18rem] md:top-12 md:right-4 md:left-auto flex flex-col justify-center w-min-[15rem] gap-4 p-6 rounded font-[1.1rem] bg-[#323232] shadow-[0_0_16px_rgba(0,0,0,.6),0_0_32px_rgba(0,0,0,.3)]'
+                >
+                    <h1 className='text-lg font-semibold'>Confirm Exit:</h1>
+                    <div>Are you sure you wish to log out?</div>
+                    <div className='flex justify-end items-center gap-6 md:gap-10 mt-2'>
+                        <button
+                            type='button'
+                            className='font-[1.1rem] font-semibold text-[#169b45] hover:text-[#1ab751] transition-colors duration-300 ease-in-out'
+                            onClick={() => setModalIsOpen(false)}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type='button'
+                            className='font-[1.1rem] font-semibold text-[#169b45] hover:text-[#1ab751] transition-colors duration-300 ease-in-out'
+                            onClick={() => 'logout'}
+                        >
+                            Yes
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default LogoutModal
