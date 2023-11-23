d79550b81d4d53ea3600dd119b3fffca95fa1e70
kdan80 <kierandansey80@gmail.com>
2023-11-23 18:25:46 +0000

    Prevent rendering routes based on isLoggedIn state - very hacky

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,8 +6,14 @@ import { useRouter } from 'next/navigation'
 const Login = () => {
     const router = useRouter()
 
+    const [isLoggedOut, setIsLoggedOut] = React.useState(false)
+
     React.useEffect(() => {
-        if (localStorage.loggedIn) return router.push('/')
+        if (localStorage.loggedIn) {
+            setIsLoggedOut(false)
+            return router.push('/')
+        }
+        setIsLoggedOut(true)
     }, [router])
 
     const username = 'guest'
@@ -24,53 +30,57 @@ const Login = () => {
     }
 
     return (
-        <main className='min-h-screen max-w-screen overflow-x-hidden'>
-            <div className='h-screen min-h-screen flex justify-center items-center overflow-hidden'>
-                <div className='h-full w-full fixed top-0 left-0 bg-[#181818] -z-10 background' />
-                <div className='w-full md:w-[500px] px-12 py-16 overflow-clip text-white md:bg-[rgba(15,15,15,.9)] md:backdrop-blur-sm md:rounded-lg md:shadow-[0_1px_16px_rgba(0,0,0,.2),0_2px_32px_rgba(0,0,0,.1)]'>
-                    <h1 className='text-3xl mb-8 font-semibold'>Sign In</h1>
+        <>
+            {isLoggedOut && (
+                <main className='min-h-screen max-w-screen overflow-x-hidden'>
+                    <div className='h-screen min-h-screen flex justify-center items-center overflow-hidden'>
+                        <div className='h-full w-full fixed top-0 left-0 bg-[#181818] -z-10 background' />
+                        <div className='w-full md:w-[500px] px-12 py-16 overflow-clip text-white md:bg-[rgba(15,15,15,.9)] md:backdrop-blur-sm md:rounded-lg md:shadow-[0_1px_16px_rgba(0,0,0,.2),0_2px_32px_rgba(0,0,0,.1)]'>
+                            <h1 className='text-3xl mb-8 font-semibold'>Sign In</h1>
 
-                    <form
-                        action=''
-                        className='flex flex-col gap-8'
-                        onSubmit={handleSubmit}
-                    >
-                        <input
-                            className='py-3 px-5 bg-[#2a2a2a] border-2 border-[#444444] rounded-md text-[#727272] italic cursor-no-drop'
-                            value={username}
-                            autoComplete={username}
-                            name='username'
-                            type='text'
-                            placeholder='Username'
-                            disabled={true}
-                            required
-                        />
+                            <form
+                                action=''
+                                className='flex flex-col gap-8'
+                                onSubmit={handleSubmit}
+                            >
+                                <input
+                                    className='py-3 px-5 bg-[#2a2a2a] border-2 border-[#444444] rounded-md text-[#727272] italic cursor-no-drop'
+                                    value={username}
+                                    autoComplete={username}
+                                    name='username'
+                                    type='text'
+                                    placeholder='Username'
+                                    disabled={true}
+                                    required
+                                />
 
-                        <input
-                            className='py-3 px-5 bg-[#2a2a2a] border-2 border-[#444444] rounded-md text-[#727272] cursor-no-drop'
-                            value={password}
-                            autoComplete={password}
-                            name='password'
-                            type='password'
-                            placeholder='Password'
-                            disabled={true}
-                            required
-                        />
+                                <input
+                                    className='py-3 px-5 bg-[#2a2a2a] border-2 border-[#444444] rounded-md text-[#727272] cursor-no-drop'
+                                    value={password}
+                                    autoComplete={password}
+                                    name='password'
+                                    type='password'
+                                    placeholder='Password'
+                                    disabled={true}
+                                    required
+                                />
 
-                        <button
-                            className='py-3 px-5 bg-[#169b45] hover:bg-[#1ab751] border border-[#169b45] hover:border-[#1ab751] rounded-md font-semibold text-[1.1rem] mb-[2rem] transition-all duration-500 ease-in-out'
-                            type='submit'
-                        >
-                            <div>Sign in as guest</div>
-                        </button>
-                    </form>
+                                <button
+                                    className='py-3 px-5 bg-[#169b45] hover:bg-[#1ab751] border border-[#169b45] hover:border-[#1ab751] rounded-md font-semibold text-[1.1rem] mb-[2rem] transition-all duration-500 ease-in-out'
+                                    type='submit'
+                                >
+                                    <div>Sign in as guest</div>
+                                </button>
+                            </form>
 
-                    <div className='text-center'>
-                        <div>Guest sign in only</div>
+                            <div className='text-center'>
+                                <div>Guest sign in only</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </main>
+                </main>
+            )}
+        </>
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export type PlayerState = 0 | 1 | 2
 export default function Home() {
     const router = useRouter()
 
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false)
     const { showFullPlayer, showMiniPlayer, showDashboard, setPlayerState } = usePlayerState()
     const { currentAlbum, loadCurrentAlbum } = useCurrentAlbum(albums)
     const { currentTrack, currentTrackSrc, setCurrentTrack } = useCurrentTrack()
@@ -27,100 +28,109 @@ export default function Home() {
         useTrackProgress()
 
     React.useEffect(() => {
-        if (!localStorage.loggedIn) return router.push('/login')
+        if (!localStorage.loggedIn) {
+            setIsLoggedIn(false)
+            return router.push('/login')
+        }
+
+        setIsLoggedIn(true)
 
         showFullPlayer
             ? document.body.classList.add('overflow-hidden')
             : document.body.classList.remove('overflow-hidden')
-    }, [showFullPlayer, router])
+    }, [showFullPlayer, router, isLoggedIn])
 
     return (
-        <main className='flex min-h-screen flex-col bg-[#101010] text-white'>
-            <Header />
-            <div
-                id='dashboard'
-                className='p-8 md:p-12'
-            >
-                {albums && (
+        <>
+            {isLoggedIn && (
+                <main className='flex min-h-screen flex-col bg-[#101010] text-white'>
+                    <Header />
                     <div
-                        id='albumGrid'
-                        className='flex flex-wrap gap-6 mb-[5rem] md:mb-[3.5rem]'
+                        id='dashboard'
+                        className='p-8 md:p-12'
                     >
-                        {albums.map((album, index) => (
-                            <AlbumCard
-                                key={index}
-                                album={album}
-                                loadCurrentAlbum={loadCurrentAlbum}
-                                setPlayerState={setPlayerState}
+                        {albums && (
+                            <div
+                                id='albumGrid'
+                                className='flex flex-wrap gap-6 mb-[5rem] md:mb-[3.5rem]'
+                            >
+                                {albums.map((album, index) => (
+                                    <AlbumCard
+                                        key={index}
+                                        album={album}
+                                        loadCurrentAlbum={loadCurrentAlbum}
+                                        setPlayerState={setPlayerState}
+                                        setCurrentTrack={setCurrentTrack}
+                                        setIsPlaying={setIsPlaying}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+
+                    <div
+                        className={`fixed  h-full w-full top-0 left-0 bg-[#080808] transition-all duration-300 ease-in-out ${
+                            showFullPlayer ? 'translate-y-auto z-20' : 'translate-y-full z-10'
+                        }`}
+                    >
+                        {currentAlbum && currentTrack && (
+                            <AudioPlayerControls
+                                currentAlbum={currentAlbum}
+                                currentTrack={currentTrack}
                                 setCurrentTrack={setCurrentTrack}
+                                setPlayerState={setPlayerState}
+                                trackProgress={trackProgress}
+                                setSkipToTimestamp={setSkipToTimestamp}
+                                isMuted={isMuted}
+                                setIsMuted={setIsMuted}
+                                volume={volume}
+                                setVolume={setVolume}
+                                isPlaying={isPlaying}
                                 setIsPlaying={setIsPlaying}
                             />
-                        ))}
+                        )}
                     </div>
-                )}
-            </div>
 
-            <div
-                className={`fixed  h-full w-full top-0 left-0 bg-[#080808] transition-all duration-300 ease-in-out ${
-                    showFullPlayer ? 'translate-y-auto z-20' : 'translate-y-full z-10'
-                }`}
-            >
-                {currentAlbum && currentTrack && (
-                    <AudioPlayerControls
-                        currentAlbum={currentAlbum}
-                        currentTrack={currentTrack}
-                        setCurrentTrack={setCurrentTrack}
-                        setPlayerState={setPlayerState}
-                        trackProgress={trackProgress}
-                        setSkipToTimestamp={setSkipToTimestamp}
-                        isMuted={isMuted}
-                        setIsMuted={setIsMuted}
-                        volume={volume}
-                        setVolume={setVolume}
-                        isPlaying={isPlaying}
-                        setIsPlaying={setIsPlaying}
-                    />
-                )}
-            </div>
+                    <div
+                        className={`fixed bottom-0 left-0 w-full transition-all duration-500 ease-in-out ${
+                            showMiniPlayer ? 'translate-y-[0] z-20' : 'translate-y-full z-10'
+                        }`}
+                    >
+                        {currentAlbum && currentTrack && (
+                            <MiniPlayer
+                                currentAlbum={currentAlbum}
+                                currentTrack={currentTrack}
+                                setCurrentTrack={setCurrentTrack}
+                                trackProgress={trackProgress}
+                                isPlaying={isPlaying}
+                                setIsPlaying={setIsPlaying}
+                                isMuted={isMuted}
+                                setIsMuted={setIsMuted}
+                                volume={volume}
+                                setVolume={setVolume}
+                                setSkipToTimestamp={setSkipToTimestamp}
+                                setPlayerState={setPlayerState}
+                            />
+                        )}
+                    </div>
 
-            <div
-                className={`fixed bottom-0 left-0 w-full transition-all duration-500 ease-in-out ${
-                    showMiniPlayer ? 'translate-y-[0] z-20' : 'translate-y-full z-10'
-                }`}
-            >
-                {currentAlbum && currentTrack && (
-                    <MiniPlayer
-                        currentAlbum={currentAlbum}
-                        currentTrack={currentTrack}
-                        setCurrentTrack={setCurrentTrack}
-                        trackProgress={trackProgress}
-                        isPlaying={isPlaying}
-                        setIsPlaying={setIsPlaying}
-                        isMuted={isMuted}
-                        setIsMuted={setIsMuted}
-                        volume={volume}
-                        setVolume={setVolume}
-                        setSkipToTimestamp={setSkipToTimestamp}
-                        setPlayerState={setPlayerState}
-                    />
-                )}
-            </div>
-
-            {/* This audio player is hidden. The code above is used for the UI */}
-            {currentAlbum && currentTrackSrc && currentTrack && (
-                <AudioPlayer
-                    currentTrackSrc={currentTrackSrc}
-                    currentAlbum={currentAlbum}
-                    currentTrack={currentTrack}
-                    setCurrentTrack={setCurrentTrack}
-                    setIsPlaying={setIsPlaying}
-                    isPlaying={isPlaying}
-                    isMuted={isMuted}
-                    volume={volume}
-                    setTrackProgress={setTrackProgress}
-                    skipToTimestamp={skipToTimestamp}
-                />
+                    {/* This audio player is hidden. The code above is used for the UI */}
+                    {currentAlbum && currentTrackSrc && currentTrack && (
+                        <AudioPlayer
+                            currentTrackSrc={currentTrackSrc}
+                            currentAlbum={currentAlbum}
+                            currentTrack={currentTrack}
+                            setCurrentTrack={setCurrentTrack}
+                            setIsPlaying={setIsPlaying}
+                            isPlaying={isPlaying}
+                            isMuted={isMuted}
+                            volume={volume}
+                            setTrackProgress={setTrackProgress}
+                            skipToTimestamp={skipToTimestamp}
+                        />
+                    )}
+                </main>
             )}
-        </main>
+        </>
     )
 }
